41306630bf5056a20246b133237b15a551a1223e
João Lopes <joao@spotify.com>
2016-05-29 01:48:20 +0200

    [Chapter13] Documentation.

--- chapter13/issues/lib/issues/table_formatter.ex
+++ chapter13/issues/lib/issues/table_formatter.ex
@@ -1,5 +1,13 @@
 defmodule Issues.TableFormatter do
 
+  @doc """
+  Takes a list of row data, where each row is a Map, and a list of headers. Prints a 
+  table to STDOUT of the data from each row identified by each header. That is, each
+  header identifies a column, and those columns are extracted and printed from the 
+  rows.0
+
+  We calculate the width of each column to fit the longest element in that column.
+  """
   def print_table_for_columns(issues, fields) do
     {clean_issues, field_widths} = extract_fields_and_widths(issues, fields)
     table_header(field_widths)
@@ -9,6 +17,12 @@ defmodule Issues.TableFormatter do
     |> Enum.each(&IO.puts/1)
   end
 
+  @doc """
+  Given a list of rows, where each row contains a keyed list of columns, return a 
+  tuple where the first element is a list of data rows containing only the columns
+  we care about and the second element is a map of the columns to the width of the
+  largest element.
+  """
   def extract_fields_and_widths(issues, fields) do
     init_field_widths = Enum.reduce(
       fields,
@@ -35,6 +49,13 @@ defmodule Issues.TableFormatter do
     {[nissue|issues], field_widths}
   end
 
+  @doc """
+  Given a value of any type return the string representation and the string length
+  of that value.
+  ## Example
+       iex> Issues.TableFormatter.to_string_width(123)
+       {"123", 3}
+  """
   def to_string_width(value) when is_binary(value), do: {value, String.length(value)}
   def to_string_width(value) do
     str_value = to_string(value)
